07cda6297cbd884eb81ad2467a4e316032627960
Buğra Aydın <bugraaydin1@hotmail.com>
2021-03-06 00:10:43 +0300

    Add Text Color & Size constants

--- config/theme.js
+++ config/theme.js
@@ -1,6 +1,16 @@
 export const IconSize = 60;
 export const Spacing = 10;
 export const BorderRadius = 16;
-export const FontSizeBody = 16;
 export const ItemWidth = 120;
 export const width = 180;
+
+//typography
+export const FontSizeTitle = 24;
+export const FontSizeSubTitle = 18;
+export const FontSizeBody = 16;
+export const FontSizeBody2 = 14;
+
+//colors
+export const TextPrimaryColor = "#555";
+export const TextSecondaryColor = "#aaa";
+export const TextLinkColor = "dodgerblue";

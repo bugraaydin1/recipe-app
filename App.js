b4fa170d166886522fee037993287af1609846ac
Buğra Aydın <bugraaydin1@hotmail.com>
2021-03-06 20:58:06 +0300

    Add navigation system and bottom tabs

--- App.js
+++ App.js
@@ -1,26 +1,71 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import SearchBox from "./components/searchBox";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { MaterialIcons } from "@expo/vector-icons/";
+import Recipes from "./screens/recipes";
 import Details from "./screens/details";
-import List from "./screens/list";
+import Favorites from "./screens/favorites";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <SearchBox />
-      <List />
-      {/* <Details /> */}
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        style={{ height: 60 }}
+        sceneContainerStyle={{ paddingBottom: 0 }}
+        initialRouteName={"Recipes"}
+        keyboardHidesTabBar={true}
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === "Tarifler") {
+              iconName = "food-bank"; //focused ? "ios-information-circle-outline"" : "ios-information-circle-outline";
+            } else if (route.name === "Favoriler") {
+              iconName = "favorite"; //format-list-bulleted //focused ? "ios-list-box" : "ios-list";
+            } else if (route.name === "Detaylar") {
+              iconName = "list"; //format-list-bulleted //focused ? "ios-list-box" : "ios-list";
+            }
+            return (
+              <MaterialIcons name={iconName} size={size * 1.2} color={color} />
+            );
+          },
+        })}
+        tabBarOptions={{
+          style: {
+            height: 55,
+            borderTopColor: "rgba(0,0,0,0.2)",
+          },
+          iconStyle: {},
+          labelStyle: {
+            fontSize: 12,
+            // paddingBottom: 11,
+          },
+          tabStyle: {
+            backgroundColor: "rgba(0,0,0,0.02)",
+            paddingTop: 5,
+            // paddingBottom: 11,
+          },
+          activeTintColor: "crimson",
+          inactiveTintColor: "gray",
+        }}
+      >
+        {/* Home */}
+        <Tab.Screen name="Tarifler" component={Recipes} />
+
+        {/* Favorites */}
+        <Tab.Screen name="Favoriler" component={Favorites} />
+
+        {/* Details */}
+        <Tab.Screen
+          name="Detaylar"
+          component={Details}
+          options={{
+            tabBarBadge: 3,
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    //flexDirection: "column",
-    //flexWrap: "wrap",
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});

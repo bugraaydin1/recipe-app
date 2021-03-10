9288077eb96953eb465a4d3151293234a5aa6a13
Buğra Aydın <bugraaydin1@hotmail.com>
2021-03-11 00:29:54 +0300

    Edit back chevron icon

--- components/backIcon.js
+++ components/backIcon.js
@@ -1,13 +1,25 @@
 import React from "react";
-import { Entypo } from "@expo/vector-icons";
-import { IconSize } from "../config/theme";
+import { Ionicons } from "@expo/vector-icons";
+import { IconSize, TinyIconSize } from "../config/theme";
 
-export const BackIcon = ({ onPress }) => {
+export const BackIcon = ({ onPress, outlined = false }) => {
   return (
-    <Entypo
-      name="chevron-thin-left"
-      size={IconSize}
-      style={{ paddingTop: 25, alignContent: "center" }}
+    <Ionicons
+      name={outlined ? "chevron-back-circle-outline" : "chevron-back"}
+      size={TinyIconSize * 1.5}
+      style={{
+        position: "absolute",
+        top: 30,
+        left: 15,
+        borderRadius: 30,
+        paddingHorizontal: 1,
+        backgroundColor: "#fff",
+        alignSelf: "flex-start",
+
+        shadowColor: "grey",
+        textShadowOffset: { height: 3, width: 3 },
+        shadowOffset: { height: 3, width: 3 },
+      }}
       color="#333"
       onPress={onPress}
     />

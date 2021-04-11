51dc73b2f435523b9ca359d03914b0b8d74aa962
Buğra Aydın <bugraaydin1@hotmail.com>
2021-04-11 20:33:36 +0300

    cleanup

--- screens/favorites.js
+++ screens/favorites.js
@@ -7,37 +7,9 @@ import { FontSizeTitle, Spacing } from "../config/theme";
 export default function Favorites() {
   const userData = useRecoilValue(userDataState);
 
-  /*  const { id, name, picture } = SocialLogin?.user || {
-    id: 0,
-    name: "",
-    picture: {},
-  }; */
-  // const loggedIn = SocialLogin.loggedIn;
-
-  console.log(
-    "##favorite user:",
-    userData?.id,
-    userData?.name || userData?.email,
-    userData?.picture || userData?.photoURL /* loggedIn */
-  );
   return (
     <SafeAreaView style={styles.mainGrid}>
       <Text style={styles.container1}>Favori Tariflerim</Text>
-
-      {(userData?.picture || userData?.photoURL) && (
-        <Image
-          style={{
-            width: 60,
-            height: 60,
-            borderRadius: 5,
-            resizeMode: "contain",
-          }}
-          source={{
-            uri: `${userData.picture?.data.url || photoURL}`,
-          }}
-        />
-      )}
-
       <View style={styles.container2}>{!userData && <SocialLogin />}</View>
     </SafeAreaView>
   );
